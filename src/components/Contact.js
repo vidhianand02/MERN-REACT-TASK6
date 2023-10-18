7d9b2591de4d2e4b51c8e31cf977f3cbea548673
Vidhi Anand <VIDHIANAND0211@GMAIL.COM>
2023-10-18 17:48:54 +0530

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
     <section id="contact">
       <h2>Contact</h2>
       <p>I'm thrilled that you've taken the time to explore my portfolio. Whether you're interested in collaborating on a project, have a question, or just want to connect, I'd love to hear from you. Feel free to reach out using any of the following methods:</p>
-      <p>Email: vermaprateek287@gmail.com</p>
+      <p>Email: vidhianand0211@gmail.com</p>
     </section>
   );
 }
