2844038bc6e834bdcae5c5e1ccd5eb740fffd607
Vidhi Anand <VIDHIANAND0211@GMAIL.COM>
2023-10-18 17:48:16 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About() {
   return (
     <section id="about">
       <h2>About Me</h2>
-      <p>Hello, I am Prateek Verma, and I'm currently pursuing a Bachelor's degree in Computer Science at VIT Bhopal. I'm passionate about technology, coding, and all things related to the digital world.</p>
+      <p>Hello, I am Vidhi Anand, and I'm currently pursuing a Bachelor's degree in Computer Science at VIT Bhopal. I'm passionate about technology, coding, and all things related to the digital world.</p>
       <p>I'm currently a proud student at VIT Bhopal, where I'm honing my skills in computer science and preparing myself for a dynamic and ever-evolving industry. My coursework has provided me with a solid foundation in various programming languages, algorithms, and data structures.</p>
       <p>Coding is not just a skill for me; it's a passion. I love tackling challenging problems and turning ideas into working software. Whether it's web development, mobile apps, or machine learning, I'm always excited to dive into new projects and learn along the way. PS: this portfolio was done under an assignment.</p>
     </section>
